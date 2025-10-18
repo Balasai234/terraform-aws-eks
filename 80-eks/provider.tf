0394cc3609dad2e626fade581dc0db7bcb1e6009
DevopsBalasai <chenibalasaivaraprasad@gmail.com>
2025-10-18 15:43:01 +0530

    S3

--- 80-eks/provider.tf
+++ 80-eks/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "latest-84s-remote-state-dev"
+    bucket = "remote-state-demo-prasad"
     key    = "roboshop-dev-eks"
     region = "us-east-1"
     encrypt        = true
